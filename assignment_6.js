a394a9c65973660afa7142f2ea34f6ac4960780f
Springbeann <77054888+Springbeann@users.noreply.github.com>
2021-06-16 00:04:11 -0400

    Create assignment_6.js

---
+++ assignment_6.js
@@ -0,0 +1,10 @@
+
+let array = ["asdf", "as", "fafeage"];
+let longest_name = 0;
+array.forEach(e => {
+  if (e.length > longest_name.length) {
+  longest_name = e;
+  }
+});
+answer = longest name;
+console.log(answer);

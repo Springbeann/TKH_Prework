c05ba2982f8aa2076ff59ab5ee651e58681b986c
Springbeann <77054888+Springbeann@users.noreply.github.com>
2021-06-15 23:56:17 -0400

    Create assignment_4.js

---
+++ assignment_4.js
@@ -0,0 +1,8 @@
+let over_under_array = 
+  [14,19,22,45,36,56,40,19]
+   let switchvalue = 25;
+for (var i=0; i<over_under_array.length; i++) {
+    if (over_under_array[i] > 25) { console.log("Over"); }
+    else if (over_under_array[i]  < 25) { console.log("Under"); }
+    else { console.log("Equal")}
+}
